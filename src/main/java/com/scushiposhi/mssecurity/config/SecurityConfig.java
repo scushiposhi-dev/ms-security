d1069e0e4dddb7ee4d3d642fb465b7d68adce9f2
scushiposhi <scushiposhi@yahoo.com>
2023-03-05 17:08:26 +0100

    first commit

---
+++ src/main/java/com/scushiposhi/mssecurity/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.scushiposhi.mssecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) ->
+                        authz.antMatchers("/").permitAll())
+                .formLogin()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+}
